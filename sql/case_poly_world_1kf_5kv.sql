1a386bc61b5700643beabf9ac18dc4433eb1de0a
Sandro Santilli <strk@keybit.net>
2014-09-29 12:12:43 +0200

    Fix case_poly_world_1kf_5kv to really have 5kv

--- sql/case_poly_world_1kf_5kv.sql
+++ sql/case_poly_world_1kf_5kv.sql
@@ -6,36 +6,41 @@ CREATE TABLE case_poly_world_1kf_5kv (
 INSERT INTO case_poly_world_1kf_5kv (the_geom_webmercator)
 WITH curve AS (
   SELECT ST_SetSRID(
-    ST_Difference(
-      ST_Rotate(
-        ST_Scale(
-          ST_Buffer(ST_MakePoint(0, 0), 10000, 32),
-          55, 40
+    ST_Translate(
+      ST_Difference(
+        ST_Rotate(
+          ST_Scale(
+            ST_Buffer(ST_MakePoint(0, 0), 10000, 256),
+            55, 40
+          ),
+          PI()/3.1,
+          ST_MakePoint(0, 0)
         ),
-        PI()/3.1,
-        ST_MakePoint(0, 0)
+        ST_Union(
+          ST_Union(
+            ST_Buffer(ST_MakePoint( 185000, 19000), 200000, 256),
+            ST_Buffer(ST_MakePoint(-220000, -4000), 100000, 256)
+          ),
+          ST_Union(
+            ST_Buffer(ST_MakePoint(-60000, -180000),   80000, 236),
+            ST_Buffer(ST_MakePoint(-120000, -330000), 120000, 384)
+          )
+        )
       ),
-      ST_Buffer(ST_MakePoint(180000, 14000), 200000, 32)
+      1200000, 1200000
     ), 3857) g
 )
 SELECT
+  --ST_Translate(st_symdifference(g, st_translate(g, -180, -140)), x, y) as the_geom_webmercator
   ST_Translate(g, x, y) as the_geom_webmercator
 FROM 
    curve,
    --generate_series(-20037500, -2003750, 2003750) x, generate_series(-20037500, -2003750, 2003750) y
-   generate_series(-20037500, -2003750, 1203750) x, generate_series(-20037500, -2003750, 2003750) y
+   generate_series(-20037500, 18000000, 1203750) x, generate_series(-20037500,  18000000, 2003750) y
 ;
 
 INSERT INTO case_poly_world_1kf_5kv(the_geom_webmercator)
-SELECT ST_Translate(the_geom_webmercator, 0, 20037500)
-FROM case_poly_world_1kf_5kv;
-
-INSERT INTO case_poly_world_1kf_5kv(the_geom_webmercator)
-SELECT ST_Translate(the_geom_webmercator, 20037500, 0)
-FROM case_poly_world_1kf_5kv;
-
-INSERT INTO case_poly_world_1kf_5kv(the_geom_webmercator)
-SELECT ST_Translate(the_geom_webmercator, 1001820, 0)
+SELECT ST_Translate(the_geom_webmercator, -400000, 900000)
 FROM case_poly_world_1kf_5kv;
 
 --INSERT INTO case_poly_world_1kf_5kv(the_geom_webmercator)
