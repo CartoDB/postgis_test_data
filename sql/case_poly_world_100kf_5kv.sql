35be025e58734cd6ac838a91a3aa4c16c7a880a9
Sandro Santilli <strk@keybit.net>
2014-09-29 12:54:52 +0200

    Fix 100kf 5kv testcase

--- sql/case_poly_world_100kf_5kv.sql
+++ sql/case_poly_world_100kf_5kv.sql
@@ -6,16 +6,30 @@ CREATE TABLE case_poly_world_100kf_5kv (
 INSERT INTO case_poly_world_100kf_5kv (the_geom_webmercator)
 WITH curve AS (
   SELECT ST_SetSRID(
-    ST_Difference(
-      ST_Rotate(
-        ST_Scale(
-          ST_Buffer(ST_MakePoint(0, 0), 10000, 32),
-          5.5, 4.0
+    ST_Scale(
+      ST_Translate(
+        ST_Difference(
+          ST_Rotate(
+            ST_Scale(
+              ST_Buffer(ST_MakePoint(0, 0), 10000, 256),
+              55, 40
+            ),
+            PI()/3.1,
+            ST_MakePoint(0, 0)
+          ),
+          ST_Union(
+            ST_Union(
+              ST_Buffer(ST_MakePoint( 185000, 19000), 200000, 256),
+              ST_Buffer(ST_MakePoint(-220000, -4000), 100000, 256)
+            ),
+            ST_Union(
+              ST_Buffer(ST_MakePoint(-60000, -180000),   80000, 236),
+              ST_Buffer(ST_MakePoint(-120000, -330000), 120000, 384)
+            )
+          )
         ),
-        PI()/3.1,
-        ST_MakePoint(0, 0)
-      ),
-      ST_Buffer(ST_MakePoint(18000, 1400), 20000, 32)
+        1200000, 1200000
+      ), 0.1, 0.1
     ), 3857) g
 )
 SELECT
@@ -23,8 +37,10 @@ SELECT
 FROM 
    curve,
    generate_series(-20037500, -133583, 133583) x,
+   --generate_series(-20037500, -20037500, 133583) x,
+
    generate_series(-20037500, -133583, 133583) y
-   --generate_series(0, 0, 980000) x, generate_series(0, 0, 18000) y
+   --generate_series(-20037500, -20037500, 133583) y
 ;
 
 INSERT INTO case_poly_world_100kf_5kv(the_geom_webmercator)
@@ -33,7 +49,13 @@ FROM case_poly_world_100kf_5kv;
 
 INSERT INTO case_poly_world_100kf_5kv(the_geom_webmercator)
 SELECT ST_Translate(the_geom_webmercator, 20037500, 0)
-FROM case_poly_world_100kf_5kv;
+FROM case_poly_world_100kf_5kv
+WHERE cartodb_id < 22500;
+
+INSERT INTO case_poly_world_100kf_5kv(the_geom_webmercator)
+SELECT ST_Translate(the_geom_webmercator, 20037500, 0)
+FROM case_poly_world_100kf_5kv
+WHERE cartodb_id >= 22500 and cartodb_id < 45000;
 
 CREATE INDEX ON case_poly_world_100kf_5kv USING GiST (the_geom_webmercator);
 
