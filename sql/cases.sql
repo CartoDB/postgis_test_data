4c1c63a871f4fdb65c96c014d79086d39fbc6ba2
Sandro Santilli <strk@keybit.net>
2014-09-29 10:52:22 +0200

    Extrapolated lines_world_1kf_5kv in its own file
    Also rotate the lines to be found in up to zoom 9

--- sql/cases.sql
+++ sql/cases.sql
@@ -69,27 +69,7 @@ FROM case_lines_world_1kf_10v;
 CREATE INDEX ON case_lines_city_1kf_10v USING GIST (the_geom_webmercator);
 
 -- lines_low_density_small_set_many_vertex
-DROP TABLE IF EXISTS case_lines_world_1kf_5kv;
-CREATE TABLE case_lines_world_1kf_5kv AS
-WITH curve AS (
-  SELECT ST_Segmentize(
-      ST_OffsetCurve(
-        ST_OffsetCurve(ST_MakeLine(g),100000)
-        ,-50000
-      ), 750) g
-  FROM (
-    SELECT ST_SetSRID(ST_MakePoint(n+(100000*cos(n)-500000*sin(n)+n/4), n/2-(n/20*cos(n))), 3857) g
-    FROM generate_series(0,2500000,100000) n
-  ) pts
-)
-SELECT
-  row_number() over() as cartodb_id,
-  ST_Translate(g, x, y) as the_geom_webmercator
-FROM 
-   curve,
-   generate_series(-20037500, 20037500, 1000000) x, generate_series(-20037500, 20037500, 1600000) y;
-CREATE INDEX ON case_lines_world_1kf_5kv USING GIST (the_geom_webmercator);
---SELECT count(*), avg(st_npoints(the_geom_webmercator)) from case_lines_world_1kf_5kv;
+\i case_lines_world_1kf_5kv.sql
 
 -- lines_high_density_small_set_many_vertex
 DROP TABLE IF EXISTS case_lines_city_1kf_5kv;
